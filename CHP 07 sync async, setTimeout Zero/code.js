233c62b552e1c5dda88034ee26058ea6093d9689
rjritesh <riteshjha24060@gmail.com>
2026-04-04 18:49:16 +0530

    Code update

--- CHP 07 sync async, setTimeout Zero/code.js
+++ CHP 07 sync async, setTimeout Zero/code.js
@@ -1,15 +1,25 @@
+const fs = require("fs");
+const https = require("https");
 console.log("Hello world");
 var a = 1078698;
 var b = 20986;
 
+https.get("https://www.example.com", (res) => {
+  console.log("fetched example.com");
+});
+
 // Runs immediately when the call stack of the main thread is empty
 setTimeout(() => {
-    console.log("call me ASAP");
+  console.log("call me ASAP");
 }, 0);
 
+fs.readFile("./file.txt", (err, data) => {
+  console.log("text file read");
+});
+
 function mulFn(x, y) {
-    const result = x * y;
-    return result;
+  const result = x * y;
+  return result;
 }
 
 var c = mulFn(a, b);
